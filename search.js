9cb960418e4e2262b74cdede749dc49193df0389
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-04-23 16:14:39 -0500

    ...

---
+++ search.js
@@ -0,0 +1,8 @@
+let searchBox = document.querySelector('#search');
+let pictures = document.querySelector('a');
+//let captions = pictures.dataset.title;
+
+for(let i=0; i<pictures.length; i++) {
+console.log(pictures[i].dataset.title);
+
+}
